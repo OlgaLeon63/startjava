1e58c25126e269747212206fa52851911c059cd9
OlgaLeon63 <logen2611@gmail.com>
2023-01-03 11:15:21 +0400

    Correction_1 HW_4_2

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -4,42 +4,19 @@ public class MyFirstGame {
         System.out.println("Игра Угадай число \n");
         int hiddenNum = 16;
         int max = 100;
-        for(int i = 100; i >= hiddenNum && i > 1; i -= 20) {
-            max = i;
-            if(hiddenNum < max) {
-            System.out.println("Число " + max + " больше того, что загадал компьютер");
-            } else if(hiddenNum > max) {
-            System.out.println("Число " + max + " меньше того, что загадал компьютер");
-            } else if(hiddenNum == max) {
-            System.out.println("Загаданное число " + hiddenNum + ". Вы победили!");
+        int min = 1;
+        while(min <= max) {
+            int mid = (min + max) / 2;
+            if(hiddenNum < mid) {
+                System.out.println("Число " + mid + " больше того, что загадал компьютер");
+                max = mid - 1;
+            } else if(hiddenNum > mid) {
+                System.out.println("Число " + mid + " меньше того, что загадал компьютер");
+                min = mid + 1;
+            } else if(hiddenNum == mid) {
+                System.out.println("Загаданное число " + hiddenNum + ". Вы победили!");
+                min = max + 1;
             }
-        }
-        for(int i = (max - 1); i >= hiddenNum && i > 0; i -= 2) {
-            max = i;
-            if(hiddenNum < max) {
-            System.out.println("Число " + max + " больше того, что загадал компьютер");
-            } else if(hiddenNum > max) {
-            System.out.println("Число " + max + " меньше того, что загадал компьютер");
-            } else if(hiddenNum == max) {
-            System.out.println("Загаданное число " + hiddenNum + ". Вы победили!");
-            }
-        }
-        for(int i = (max + 1); i <= hiddenNum && i > 0; i += 2) {
-            max = i;
-            if(hiddenNum < max) {
-            System.out.println("Число " + max + " больше того, что загадал компьютер");
-            } else if(hiddenNum > max) {
-            System.out.println("Число " + max + " меньше того, что загадал компьютер");
-            } else if(hiddenNum == max) {
-            System.out.println("Загаданное число " + hiddenNum + ". Вы победили!");
-            }
-        }
-        if(hiddenNum > max) {
-            max++;
-            System.out.println("Загаданное число " + hiddenNum + ". Вы победили!");
-        } else if(hiddenNum < max && hiddenNum > 1) {
-            max--;
-            System.out.println("Загаданное число " + hiddenNum + ". Вы победили!");
         }
     }
 }
