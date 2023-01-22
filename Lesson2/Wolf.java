3e0204713c66107b6d343a8ef0183d59fa88c131
OlgaLeon63 <logen2611@gmail.com>
2023-01-22 20:40:56 +0400

    Implement Lesson2 HW_3

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,32 +1,76 @@
 public class Wolf {
 
-    String gender;
-    String nickname;
-    float weight;
-    short age;
-    String color;
+    private String gender;
+    private String nickname;
+    private float weight;
+    private int age;
+    private String color;
 
-    boolean move() {
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean move() {
         System.out.println(nickname + " идет");
         return true;
     }
 
-    boolean sit() {
+    public boolean sit() {
         System.out.println(nickname + " сидит");
         return true;
     }
 
-    boolean run() {
+    public boolean run() {
         System.out.println(nickname + " бежит");
         return true;
     }
 
-    boolean howl() {
+    public boolean howl() {
         System.out.println(nickname + " воет");
         return true;
     }
 
-    boolean hunt() {
+    public boolean hunt() {
         System.out.println(nickname + " охотится");
         return true;
     }
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.gender = "male";
-        wolfOne.nickname = "Mike";
-        wolfOne.weight = 1.6f;
-        wolfOne.age = 10;
-        wolfOne.color = "grey";
-        System.out.println("gender = " + wolfOne.gender);
-        System.out.println("nickname = " + wolfOne.nickname);
-        System.out.println("weight = " + wolfOne.weight);
-        System.out.println("age = " + wolfOne.age);
-        System.out.println("color = " + wolfOne.color);
+        wolfOne.setGender("male");
+        wolfOne.setNickname("Mike");
+        wolfOne.setWeight(1.6f);
+        wolfOne.setAge(10);
+        wolfOne.setColor("grey");
+        System.out.println("gender = " + wolfOne.getGender());
+        System.out.println("nickname = " + wolfOne.getNickname());
+        System.out.println("weight = " + wolfOne.getWeight());
+        System.out.println("age = " + wolfOne.getAge());
+        System.out.println("color = " + wolfOne.getColor());
         System.out.println(wolfOne.move());
         System.out.println(wolfOne.sit());
         System.out.println(wolfOne.run());
